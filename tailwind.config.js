7cea0f94bed204b1a1165ec7518787dc51220269
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-25 00:30:11 +0900

    feat: tailwind font setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      'headline-30': ['Headline-30', 'sans'],
+      'headline-25': ['Headline-25', 'sans'],
+      'subhead-18': ['SubHead-18', 'sans'],
+      'subhead-16': ['SubHead-16', 'sans'],
+      'subhead-13': ['SubHead-13', 'sans'],
+      'body-18': ['Body-18', 'sans'],
+      'body-16': ['Body-16', 'sans'],
+      'body-15': ['Body-15', 'sans'],
+      'body-14': ['Body-14', 'sans'],
+      'caption-13': ['Caption-13', 'sans'],
+    },
     extend: {},
   },
   plugins: [],
