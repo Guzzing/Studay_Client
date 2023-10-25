24ea824b388c225d7c5ef16dfdb3f6f9bfa193cc
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-25 12:00:09 +0900

    feat: 사용할 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,12 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    fontFamily: {
-      'headline-30': ['Headline-30', 'sans'],
-      'headline-25': ['Headline-25', 'sans'],
-      'subhead-18': ['SubHead-18', 'sans'],
-      'subhead-16': ['SubHead-16', 'sans'],
-      'subhead-13': ['SubHead-13', 'sans'],
-      'body-18': ['Body-18', 'sans'],
-      'body-16': ['Body-16', 'sans'],
-      'body-15': ['Body-15', 'sans'],
-      'body-14': ['Body-14', 'sans'],
-      'caption-13': ['Caption-13', 'sans'],
+    extend: {
+      fontFamily: {
+        // TODO : font-nsk로 바로 폰트 사용가능합니다!
+        nsk: ['Noto Sans KR', 'cursive'],
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
