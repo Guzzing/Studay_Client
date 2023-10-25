6b254d0ba7cfd2067e487aa01a39207b814bf36a
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-25 12:09:25 +0900

    refactor: App.tsx 폰트 테스트 내용 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,16 @@ import './styles/index.css'
 function App() {
   return (
     <div className={'App'}>
-      <p className={'headline-30'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'headline-25'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'subHead-18'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'subHead-16'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'subHead-13'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'body-18'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'body-16'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'body-15'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'body-14'}>{'안녕하세요! 기동팀입니다'}</p>
-      <p className={'caption-13'}>{'안녕하세요! 기동팀입니다'}</p>
-      {/* 다른 내용 추가 */}
+      <p className={'headline-30'}>{'headline-30 font'}</p>
+      <p className={'headline-25'}>{'headline-25 font'}</p>
+      <p className={'subHead-18'}>{'subHead-18 font'}</p>
+      <p className={'subHead-16'}>{'subHead-16 font'}</p>
+      <p className={'subHead-13'}>{'subHead-13 font'}</p>
+      <p className={'body-18'}>{'body-18 font'}</p>
+      <p className={'body-16'}>{'body-16 font'}</p>
+      <p className={'body-15'}>{'body-15 font'}</p>
+      <p className={'body-14'}>{'body-14 font'}</p>
+      <p className={'caption-13'}>{'caption-13 font'}</p>
     </div>
   )
 }
