1638a5781bbd6b9b9639cc42433029b0e5778955
Eugene Kim <eugene02876@gmail.com>
2023-10-25 18:19:48 +0900

    chore: 스토리북 쓸데없는 주석 제거

--- src/components/common/Button.stories.ts
+++ src/components/common/Button.stories.ts
@@ -4,7 +4,6 @@ const meta = {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
   tags: ['autodocs'],
