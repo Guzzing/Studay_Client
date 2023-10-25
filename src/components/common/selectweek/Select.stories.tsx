99b040e1ecf4baef28b9a27be3783edbc522f3a9
Eugene Kim <eugene02876@gmail.com>
2023-10-26 03:04:47 +0900

    feat: selectWeek 컴포넌트 생성

--- src/components/common/selectweek/Select.stories.tsx
+++ src/components/common/selectweek/Select.stories.tsx
@@ -1,13 +1,45 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { useEffect, useState } from 'react'
 import SelectWeek from './SelectWeek'
 
 const meta: Meta<typeof SelectWeek> = {
-  component: SelectWeek
+  component: SelectWeek,
+  title: 'Atom/SelectWeek',
+  tags: ['autodocs'],
+  argTypes: {
+    fixedDate: {
+      control: { type: 'number', min: 0, max: 6 }
+    },
+    selectedDate: {
+      control: 'multi-select',
+      options: [0, 1, 2, 3, 4, 5, 6]
+    },
+    setSelectedDate: {
+      control: { type: 'function' }
+    }
+  },
+  render: function Render(args) {
+    const [selectedDate, setSelectedDate] = useState<number[]>([])
+    useEffect(() => {
+      setSelectedDate([...args.selectedDate])
+    }, [args.selectedDate])
+    return (
+      <SelectWeek
+        setSelectedDate={setSelectedDate}
+        selectedDate={selectedDate}
+        fixedDate={args.fixedDate}
+      />
+    )
+  }
 }
 
 export default meta
 type Story = StoryObj<typeof SelectWeek>
 
 export const Default: Story = {
-  render: () => <SelectWeek />
+  ...meta,
+  args: {
+    fixedDate: 0,
+    selectedDate: [1]
+  }
 }
--- src/components/common/selectweek/SelectWeek.tsx
+++ src/components/common/selectweek/SelectWeek.tsx
@@ -1,13 +1,63 @@
-const SelectWeek = () => {
+/* eslint-disable prettier/prettier */
+import { cva } from 'class-variance-authority'
+import cn from '../../../libs/utils/cn'
+
+export const SelectWeekVariant = cva(
+  `flex justify-center items-center w-[39px] h-[39px]`,
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-white-100 rounded-full body-16 text-black-900 border-blue-500',
+        selected: 'bg-blue-500 rounded-full body-16 text-white-100',
+        fixed: 'disabled: bg-gray-400 rounded-full body-16 text-white-100'
+      },
+      defaultVariants: {
+        variant: 'default'
+      }
+    }
+  }
+)
+
+interface SelectWeekProperties {
+  fixedDate?: number
+  selectedDate: number[]
+  setSelectedDate: React.Dispatch<React.SetStateAction<number[]>>
+}
+
+const SelectWeek = ({
+  fixedDate,
+  setSelectedDate,
+  selectedDate
+}: SelectWeekProperties) => {
+  const week = ['일', '월', '화', '수', '목', '금', '토']
   return (
     <div className={'flex flex-row gap-2.5'}>
-      <button>{'월'}</button>
-      <button>{'화'}</button>
-      <button>{'수'}</button>
-      <button>{'목'}</button>
-      <button>{'금'}</button>
-      <button>{'토'}</button>
-      <button>{'일'}</button>
+      {week.map((day, index) => (
+        <button
+          key={index}
+          className={cn(
+            SelectWeekVariant({
+              variant:
+                fixedDate === index
+                  ? 'fixed'
+                  : selectedDate.includes(index)
+                    ? 'selected'
+                    : 'default'
+            })
+          )}
+          onClick={() => {
+            if (selectedDate.includes(index)) {
+              const filteredDate = selectedDate.filter((data) => data !== index)
+              setSelectedDate([...filteredDate])
+            } else {
+              setSelectedDate([...selectedDate, index])
+            }
+          }}
+        >
+          {day}
+        </button>
+      ))}
     </div>
   )
 }
