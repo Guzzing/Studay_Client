6b473e57bf52a4f4d8e42e78f13252c7d40af531
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-25 12:02:00 +0900

    feat: app.tsx에 폰트 test

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href={'https://vitejs.dev'} target={'_blank'}>
-          <img src={viteLogo} className={'logo'} alt={'Vite logo'} />
-        </a>
-        <a href={'https://react.dev'} target={'_blank'}>
-          <img src={reactLogo} className={'logo react'} alt={'React logo'} />
-        </a>
-      </div>
-      <h1>{'Vite + React'}</h1>
-      <div className={'card'}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          {'count is '}
-          {count}
-        </button>
-        <p>
-          {'Edit '}
-          <code>{'src/App.tsx'}</code> {'and save to test HMR'}
-        </p>
-      </div>
-      <p className={'read-the-docs'}>
-        {'Click on the Vite and React logos to learn more'}
-      </p>
-    </>
+    <div className={'App'}>
+      <p className={'headline-30'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'headline-25'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'subHead-18'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'subHead-16'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'subHead-13'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'body-18'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'body-16'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'body-15'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'body-14'}>{'안녕하세요! 기동팀입니다'}</p>
+      <p className={'caption-13'}>{'안녕하세요! 기동팀입니다'}</p>
+      {/* 다른 내용 추가 */}
+    </div>
   )
 }
 
