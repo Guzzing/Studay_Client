9cb8ec75196a39dac26f058738b332e918187402
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-27 02:45:27 +0900

    feat: 컴포넌트 유연함을 위해 모든 속성을 각각 구분

---
+++ src/components/common/button/constants.ts
@@ -0,0 +1,39 @@
+export const FONT_STYLE = {
+  NSK: 'font-nsk'
+}
+
+export const BUTTON_RADIUS = {
+  min: 'rounded-none',
+  middle: 'rounded-12',
+  max: 'rounded-full'
+}
+
+export const BUTTON_BORDER_COLOR = {
+  red600: 'border-red-600',
+  blue500: 'border-blue-500'
+}
+
+export const BUTTON_BG_COLOR = {
+  white0: 'bg-white-0',
+  blue500: 'bg-blue-500',
+  blue700: 'bg-blue-700'
+}
+
+export const BUTTON_WIDTH = {
+  SW: 'w-60px',
+  MW: 'w-124px',
+  LW: 'w-343px',
+  XLW: 'w-390px'
+}
+
+export const BUTTON_HEIGHT = {
+  SH: 'w-43px',
+  MH: 'w-56px',
+  LH: 'w-60px'
+}
+
+export const BUTTON_TEXT_COLOR = {
+  white0: 'text-white-0',
+  blue500: 'text-blue-500',
+  red600: 'text-red-600'
+}
