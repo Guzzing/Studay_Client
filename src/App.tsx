5c263e5109aa4a4102fb948508e09465cac93710
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-26 23:26:33 +0900

    fix: font error해결 => font-nsk className추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './styles/index.css'
 function App() {
   return (
     <div className={'App'}>
-      <p className={'headline-30'}>{'headline-30 font'}</p>
-      <p className={'headline-25'}>{'headline-25 font'}</p>
-      <p className={'subHead-18'}>{'subHead-18 font'}</p>
-      <p className={'subHead-16'}>{'subHead-16 font'}</p>
-      <p className={'subHead-13'}>{'subHead-13 font'}</p>
-      <p className={'body-18'}>{'body-18 font'}</p>
-      <p className={'body-16'}>{'body-16 font'}</p>
-      <p className={'body-15'}>{'body-15 font'}</p>
-      <p className={'body-14'}>{'body-14 font'}</p>
-      <p className={'caption-13'}>{'caption-13 font'}</p>
+      <p className={'headline-30 font-nsk'}>{'headline-30 font'}</p>
+      <p className={'headline-25 font-nsk'}>{'headline-25 font'}</p>
+      <p className={'subHead-18 font-nsk'}>{'subHead-18 font'}</p>
+      <p className={'subHead-16 font-nsk'}>{'subHead-16 font'}</p>
+      <p className={'subHead-13 font-nsk'}>{'subHead-13 font'}</p>
+      <p className={'body-18 font-nsk'}>{'body-18 font'}</p>
+      <p className={'body-16 font-nsk'}>{'body-16 font'}</p>
+      <p className={'body-15 font-nsk'}>{'body-15 font'}</p>
+      <p className={'body-14 font-nsk'}>{'body-14 font'}</p>
+      <p className={'caption-13 font-nsk'}>{'caption-13 font'}</p>
     </div>
   )
 }
