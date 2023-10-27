716e95e20fc62ef4c038d7f96e214d49d10d2cc6
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-27 11:27:17 +0900

    feat: button props에서 size삭제 => width, height로 대체

--- src/components/common/button/ButtonType.ts
+++ src/components/common/button/ButtonType.ts
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 
-export type ButtonSize = 'S' | 'M' | 'L'
 export type ButtonTextColor = 'blue500' | 'white0' | 'red600'
 export type ButtonBgColor = 'blue500' | 'blue700' | 'white0'
 export type ButtonBorderColor = 'blue500' | 'red600'
@@ -11,9 +10,8 @@ export type ButtonHeight = 'SH' | 'MH' | 'LH'
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode
+  label: ReactNode
   buttonType?: ButtonType
-  size?: ButtonSize
   textColor?: ButtonTextColor
   bgColor?: ButtonBgColor
   borderColor?: ButtonBorderColor
