7a8655cdc65242bc889a44c7b4a28d6c2d05a2c8
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-27 15:41:45 +0900

    feat: buttonType 추가

--- src/components/common/button/ButtonType.ts
+++ src/components/common/button/ButtonType.ts
@@ -3,7 +3,13 @@ import { ReactNode } from 'react'
 export type ButtonTextColor = 'blue500' | 'white0' | 'red600'
 export type ButtonBgColor = 'blue500' | 'blue700' | 'white0'
 export type ButtonBorderColor = 'blue500' | 'red600'
-export type ButtonType = 'Plain' | 'Round' | 'Square' | 'Floating'
+export type ButtonType =
+  | 'Plain-blue'
+  | 'Plain-red'
+  | 'Round-blue-500'
+  | 'Round-blue-700'
+  | 'Square'
+  | 'Floating'
 export type ButtonBorderRadius = 'min' | 'middle' | 'max'
 export type ButtonWidth = 'SW' | 'MW' | 'LW' | 'XLW'
 export type ButtonHeight = 'SH' | 'MH' | 'LH'
@@ -11,12 +17,13 @@ export type ButtonHeight = 'SH' | 'MH' | 'LH'
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: ReactNode
-  buttonType?: ButtonType
+  buttonType: ButtonType
   textColor?: ButtonTextColor
   bgColor?: ButtonBgColor
   borderColor?: ButtonBorderColor
   borderRadius?: ButtonBorderRadius
   width?: ButtonWidth
   height?: ButtonHeight
+  fullWidth?: boolean
   onClick?: () => void
 }
