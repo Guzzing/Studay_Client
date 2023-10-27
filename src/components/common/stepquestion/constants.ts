aaa12ce2c04b6b3b4d23cb189e6007393d024851
kimheeseok <kim1140129@gmail.com>
2023-10-27 15:02:15 +0900

    feat: StepQuestion 구현

---
+++ src/components/common/stepquestion/constants.ts
@@ -0,0 +1,4 @@
+export const StepQuestionContainer = 'flex items-center'
+export const StepStyle =
+  'w-[28px] h-[28px] text-center bg-blue-500 rounded-full body-16 font-nsk leading-[24px] font-bold text-white-0 mr-[10px]'
+export const TextStyle = 'font-nsk body-17'
