a785541766e12c6f6ec71df70874c78661e76a19
Eugene Kim <eugene02876@gmail.com>
2023-10-29 02:35:33 +0900

    fix: Detailcontent와 margin-bottom값 주어지기

--- src/components/BottomSheet/BottomSheetContent.tsx
+++ src/components/BottomSheet/BottomSheetContent.tsx
@@ -1,9 +1,9 @@
 const BottomSheetContent = ({ expanded }: { expanded: boolean }) => {
   return (
     <div>
-      <span className={'font-nsk body-15 text-black-900'}>
+      <div className={'font-nsk body-15 text-black-900 mb-[20px]'}>
         {' 서울 강동구 상암로 12 202호 (우) 05242 02-426-3688 '}
-      </span>
+      </div>
       {expanded && <div>{'펼쳤을 때 나오는 어쩌구 '}</div>}
     </div>
   )
