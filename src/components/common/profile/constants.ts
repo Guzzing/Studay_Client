42c4d418c9d11422cf8585f3f2a0e387a5444816
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-30 17:56:17 +0900

    feat: Profile 컴포넌트 사이즈 추가

--- src/components/common/profile/constants.ts
+++ src/components/common/profile/constants.ts
@@ -1,7 +1,8 @@
 export const PROFILE_SIZE = {
-  L: 'w-[150px] h-[150px]',
-  M: 'w-[92px] h-[92px]',
-  S: 'w-[70px] h-[70px]'
+  XL: 'w-[150px] h-[150px]',
+  L: 'w-[92px] h-[92px]',
+  M: 'w-[70px] h-[70px]',
+  S: 'w-[28px] h-[28px]'
 }
 export const PROFILE_BORDER_COLOR = {
   black500: 'border-black-500'
