231c8128f6c3a47c88bd484bbfefef322377fb33
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-30 13:01:00 +0900

    feat: input stories작성

---
+++ src/components/common/inputbox/input/Input.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
+import Input from '@/components/common/inputbox/input/Input'
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  tags: ['autodocs'],
+  title: 'components/Input',
+  argTypes: {
+    inputType: {
+      control: 'select',
+      options: ['Default', 'Select']
+    },
+    fullWidth: {
+      control: 'boolean'
+    },
+    value: { control: 'text' },
+    placeholder: { control: 'text' }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof Input>
+
+export const DefaultInput: Story = {
+  args: {
+    inputType: 'Default',
+    fullWidth: true,
+    placeholder: '값을 입력해주세요'
+  }
+}
+
+export const SearchInput: Story = {
+  args: {
+    inputType: 'Search',
+    fullWidth: true,
+    placeholder: '값을 입력해주세요'
+  }
+}
