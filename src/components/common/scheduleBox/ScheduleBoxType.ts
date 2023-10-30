87be68fc8263675051b7b49a50530645bd579a38
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-30 16:36:10 +0900

    feat: rightTopElement고정

--- src/components/common/scheduleBox/ScheduleBoxType.ts
+++ src/components/common/scheduleBox/ScheduleBoxType.ts
@@ -4,8 +4,7 @@ type ScheduleType = 'toggle' | 'profile'
 export interface ScheduleBoxProps {
   scheduleType: ScheduleType
   mainTitle: string
-  subElement?: ReactNode // HTML태그를 넣을 수 있음!
+  subElement: ReactNode // HTML태그를 넣을 수 있음!
   rightBottomElement?: ReactNode // HTML태그를 넣을 수 있음!
-  rightTopElement?: ReactNode // HTML!
   isRegister?: boolean
 }
