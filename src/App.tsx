f2299601dfeeff74c057d954af56fc27ffc08a85
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-30 21:29:34 +0900

    feat: RouterProvider로 감싸기

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 import './styles/index.css'
 function App() {
-  return (
-    <div className={'bg-blue-200'}>
-      <p className={'headline-30'}>{'headline-30 font'}</p>
-      <p className={'headline-25'}>{'headline-25 font'}</p>
-      <p className={'subHead-18'}>{'subHead-18 font'}</p>
-      <p className={'subHead-16'}>{'subHead-16 font'}</p>
-      <p className={'subHead-13'}>{'subHead-13 font'}</p>
-      <p className={'body-18'}>{'body-18 font'}</p>
-      <p className={'body-16'}>{'body-16 font'}</p>
-      <p className={'body-15'}>{'body-15 font'}</p>
-      <p className={'body-14'}>{'body-14 font'}</p>
-      <p className={'caption-13'}>{'caption-13 font'}</p>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
