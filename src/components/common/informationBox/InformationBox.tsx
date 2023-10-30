b556ea361faf5b1df41467ae4111b3e83bdd9e06
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-30 17:24:53 +0900

    feat: border적용, icon, profile컴포넌트 적용

--- src/components/common/informationBox/InformationBox.tsx
+++ src/components/common/informationBox/InformationBox.tsx
@@ -1,9 +1,10 @@
 import type { InformationBoxProps } from './InformationBoxType'
+import Icon from '../icon/Icon'
+import Profile from '../profile/Profile'
 
 // CHECK
 // Profile component가 머지되면 바로 프로필 컴포넌트 반영할게요!
 const InformationBox = ({
-  image = 'https://chanwookim.me/agumon-dday/agumon.png',
   mainTitle,
   subTitle,
   description // CHECK : description은 글자 수 설정을 해놓으면 좋을 것 같습니다!
@@ -11,17 +12,11 @@ const InformationBox = ({
   return (
     <div
       className={
-        'p-[14px] w-[345px] h-[120px] border border-black-900 font-nsk overflow-hidden rounded-[10px]'
+        'p-[14px] w-[345px] h-[120px] border border-black-900 font-nsk overflow-hidden rounded-[10px] bg-white-200'
       }
     >
       <div className={'w-full h-full flex'}>
-        <div className={'relative w-[92px] h-[92px]'}>
-          <img
-            src={image}
-            alt={'informationBox image'}
-            className={'w-full h-full'}
-          />
-        </div>
+        <Profile imageSize={'M'} canEdit={true} />
         <div
           className={
             'relative w-[240px] h-full pl-[17px] flex flex-col justify-between'
@@ -32,9 +27,8 @@ const InformationBox = ({
           <p className={'text-black-900 caption-13 break-words'}>
             {description}
           </p>
-          {/* CHECK : 아이콘 컴포넌트 미반영 */}
           <span className={'absolute top-[3px] right-[3px] cursor-pointer'}>
-            {'✏️'}
+            <Icon icon={'Edit'} classStyle={'text-black-900'} />
           </span>
         </div>
       </div>
