77cacee4e4bed4bb262967ad5da8f93ec94def97
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-31 15:12:20 +0900

    informationBox 컴포넌트 개발 (#46)
    * feat: Information 스토리북 설정
    
    * feat: InformationBox 컴포넌트 개발
    
    * feat: InformationBox type 세팅
    
    * feat: informationBox 편집 icon개발
    
    * feat: InformationBox rounded-[10px]적용
    
    * feat: border적용, icon, profile컴포넌트 적용
    
    * feat: informationBox border제거

---
+++ src/components/common/informationBox/InformationBox.tsx
@@ -0,0 +1,39 @@
+import type { InformationBoxProps } from './InformationBoxType'
+import Icon from '../icon/Icon'
+import Profile from '../profile/Profile'
+
+// CHECK
+// Profile component가 머지되면 바로 프로필 컴포넌트 반영할게요!
+const InformationBox = ({
+  mainTitle,
+  subTitle,
+  description // CHECK : description은 글자 수 설정을 해놓으면 좋을 것 같습니다!
+}: InformationBoxProps) => {
+  return (
+    <div
+      className={
+        'p-[14px] w-[345px] h-[120px] font-nsk overflow-hidden rounded-[10px] bg-white-200'
+      }
+    >
+      <div className={'w-full h-full flex'}>
+        <Profile imageSize={'M'} canEdit={true} />
+        <div
+          className={
+            'relative w-[240px] h-full pl-[17px] flex flex-col justify-between'
+          }
+        >
+          <p className={'subHead-18 font-nsk text-black-900'}>{mainTitle}</p>
+          <p className={'text-gray-700 body-15'}>{subTitle}</p>
+          <p className={'text-black-900 caption-13 break-words'}>
+            {description}
+          </p>
+          <span className={'absolute top-[3px] right-[3px] cursor-pointer'}>
+            <Icon icon={'Edit'} classStyle={'text-black-900'} />
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InformationBox
