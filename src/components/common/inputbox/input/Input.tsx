308356b4d2d5f5283d4b8ad76bc9c6d6807eac65
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-31 15:47:40 +0900

    feat: ref props적용

--- src/components/common/inputbox/input/Input.tsx
+++ src/components/common/inputbox/input/Input.tsx
@@ -1,48 +1,57 @@
 import type { InputProps } from './InputType'
+import { forwardRef } from 'react'
 import Icon from '../../icon/Icon'
 
-const Input = ({
-  inputType = 'Default',
-  value = '',
-  fullWidth = true,
-  width,
-  height,
-  name,
-  placeholder,
-  ...props
-}: InputProps) => {
-  return inputType === 'Default' ? (
-    <input
-      type={'text'}
-      className={`${
-        fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
-      } rounded-[10px] border border-blue-500 px-[20px] font-nsk text-gray-600 bg-white-200
-      placeholder:text-gray-600 outline-none
-      `}
-      value={value}
-      name={name}
-      placeholder={placeholder}
-      {...props}
-    />
-  ) : inputType === 'Search' ? (
-    <div
-      className={`${
-        fullWidth ? 'w-[298px] h-[50px]' : `w-[${width}px] h-[${height}px]`
-      } rounded-[30px] border border-blue-500 px-[20px] flex items-center bg-white-200`}
-    >
-      <Icon icon={'Search'} classStyle={'text-black-900'} />
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      inputType = 'Default',
+      value = '',
+      fullWidth = true,
+      width,
+      height,
+      name,
+      placeholder,
+      onChange,
+      ...props
+    }: InputProps,
+    ref
+  ) => {
+    return inputType === 'Default' ? (
       <input
-        type={'search'}
-        className={`grow h-full w-full px-[5px] font-nsk text-gray-600 bg-white-200 outline-none`}
+        type={'text'}
+        className={`${
+          fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
+        } rounded-[10px] border border-blue-500 px-[20px] font-nsk text-gray-600 bg-white-200 placeholder:text-gray-600 outline-none`}
         value={value}
         name={name}
         placeholder={placeholder}
+        onChange={onChange}
+        ref={ref}
         {...props}
       />
-    </div>
-  ) : (
-    ''
-  )
-}
+    ) : inputType === 'Search' ? (
+      <div
+        className={`${
+          fullWidth ? 'w-[298px] h-[50px]' : `w-[${width}px] h-[${height}px]`
+        } rounded-[30px] border border-blue-500 px-[20px] flex items-center bg-white-200`}
+      >
+        <Icon icon={'Search'} classStyle={'text-black-900'} />
+        <input
+          type={'search'}
+          className={`grow h-full w-full px-[5px] font-nsk text-gray-600 bg-white-200 outline-none`}
+          value={value}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    ) : (
+      ''
+    )
+  }
+)
 
 export default Input
--- src/components/common/inputbox/select/Select.tsx
+++ src/components/common/inputbox/select/Select.tsx
@@ -1,50 +1,59 @@
 import type { SelectProps } from './SelectType'
+import React, { forwardRef } from 'react'
 
-const Select = ({
-  selectType = 'Single',
-  fullWidth = true,
-  name,
-  value = '',
-  width,
-  height,
-  ...props
-}: SelectProps) => {
-  return selectType === 'Single' ? (
-    <select
-      name={name}
-      id={''}
-      className={`${
-        fullWidth ? `w-[324px] h-[52px]` : `w-[${width}px] h-[${height}px]`
-      } font-nsk body-18 px-[20px] border border-blue-500 rounded-[10px] outline-none bg-white-200
-    text-gray-600
-    `}
-    >
-      <option value={value} className={'bg-blue-500'}>
-        {'서울시'}
-      </option>
-    </select>
-  ) : (
-    <div
-      className={`px-[23px] ${
-        fullWidth ? 'w-[350px] h-[44px]' : `w-[${width}px] h-[${height}px]`
-      } flex justify-between items-center border border-blue-500 rounded-[10px] font-nsk
-      bg-white-200
-      `}
-    >
-      <p className={'body-18 text-black-900'}>{'반복'}</p>
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  (
+    {
+      selectType = 'Single',
+      fullWidth = true,
+      name,
+      value = '',
+      width,
+      height,
+      ...props
+    }: SelectProps,
+    ref
+  ) => {
+    return selectType === 'Single' ? (
       <select
+        ref={ref}
         name={name}
         id={''}
-        className={
-          'bg-white-200 body-14 text-gray-600 outline-none cursor-pointer relative'
-        }
+        className={`${
+          fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
+        } font-nsk body-18 px-[20px] border border-blue-500 rounded-[10px] outline-none bg-white-200
+      text-gray-600
+      `}
         {...props}
       >
-        <option value={'true'}>{'설정'}</option>
-        <option value={'false'}>{'미설정'}</option>
+        <option value={value} className={'bg-blue-500'}>
+          {'서울시'}
+        </option>
       </select>
-    </div>
-  )
-}
+    ) : (
+      <div
+        className={`px-[23px] ${
+          fullWidth ? 'w-[350px] h-[44px]' : `w-[${width}px] h-[${height}px]`
+        } flex justify-between items-center border border-blue-500 rounded-[10px] font-nsk
+        bg-white-200
+        `}
+      >
+        <p className={'body-18 text-black-900'}>{'반복'}</p>
+        <select
+          ref={ref}
+          name={name}
+          id={''}
+          className={
+            'bg-white-200 body-14 text-gray-600 outline-none cursor-pointer relative'
+          }
+          {...props}
+        >
+          <option value={'true'}>{'설정'}</option>
+          <option value={'false'}>{'미설정'}</option>
+        </select>
+      </div>
+    )
+  }
+)
 
 export default Select
