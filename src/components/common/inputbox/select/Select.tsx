937b8aa29863e3b504926aebd4eb8bdf7fb342fd
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-31 16:53:46 +0900

    feat: select컴포넌트 pr리뷰 반영

--- src/components/common/inputbox/select/Select.tsx
+++ src/components/common/inputbox/select/Select.tsx
@@ -1,5 +1,6 @@
 import type { SelectProps } from './SelectType'
-import React, { forwardRef } from 'react'
+import { forwardRef } from 'react'
+import Icon from '../../icon/Icon'
 
 const Select = forwardRef<HTMLSelectElement, SelectProps>(
   (
@@ -7,51 +8,63 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
       selectType = 'Single',
       fullWidth = true,
       name,
-      value = '',
+      value = [],
       width,
       height,
+      errorMessage,
+      onChange,
       ...props
     }: SelectProps,
     ref
   ) => {
     return selectType === 'Single' ? (
-      <select
-        ref={ref}
-        name={name}
-        id={''}
-        className={`${
-          fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
-        } font-nsk body-18 px-[20px] border border-blue-500 rounded-[10px] outline-none bg-white-200
-      text-gray-600
-      `}
-        {...props}
-      >
-        <option value={value} className={'bg-blue-500'}>
-          {'서울시'}
-        </option>
-      </select>
-    ) : (
-      <div
-        className={`px-[23px] ${
-          fullWidth ? 'w-[350px] h-[44px]' : `w-[${width}px] h-[${height}px]`
-        } flex justify-between items-center border border-blue-500 rounded-[10px] font-nsk
-        bg-white-200
-        `}
-      >
-        <p className={'body-18 text-black-900'}>{'반복'}</p>
+      <>
         <select
           ref={ref}
           name={name}
           id={''}
-          className={
-            'bg-white-200 body-14 text-gray-600 outline-none cursor-pointer relative'
-          }
+          className={`${
+            fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
+          } font-nsk body-18 px-[20px] border border-blue-500 rounded-[10px] outline-none bg-white-200
+      text-gray-600
+      `}
+          onChange={onChange}
           {...props}
         >
-          <option value={'true'}>{'설정'}</option>
-          <option value={'false'}>{'미설정'}</option>
+          {value.map((optionValue) => (
+            <option value={optionValue}>{optionValue}</option>
+          ))}
         </select>
-      </div>
+        <Icon icon={'ArrowDown'} />
+        <p className={'px-[10px] text-red-600'}>{errorMessage}</p>
+      </>
+    ) : (
+      <>
+        <div
+          className={`px-[23px] ${
+            fullWidth ? 'w-[350px] h-[44px]' : `w-[${width}px] h-[${height}px]`
+          } flex justify-between items-center border border-blue-500 rounded-[10px] font-nsk
+        bg-white-200
+        `}
+        >
+          <p className={'body-18 text-black-900'}>{'반복'}</p>
+          <select
+            ref={ref}
+            name={name}
+            id={''}
+            className={
+              'bg-white-200 body-14 text-gray-600 outline-none cursor-pointer relative'
+            }
+            onChange={onChange}
+            {...props}
+          >
+            {value.map((optionValue) => (
+              <option value={optionValue}>{optionValue}</option>
+            ))}
+          </select>
+        </div>
+        <p className={'px-[10px] text-red-600'}>{errorMessage}</p>
+      </>
     )
   }
 )
--- src/components/common/inputbox/select/SelectType.ts
+++ src/components/common/inputbox/select/SelectType.ts
@@ -4,8 +4,9 @@ type SelectType = 'Single' | 'Box'
 export interface SelectProps extends ComponentProps<'select'> {
   selectType: SelectType
   fullWidth: boolean
-  value: string
+  value: string[]
   width?: number
   height?: number
+  errorMessage?: string
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
