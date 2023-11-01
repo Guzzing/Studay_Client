5a23149899ce482ca3b32cb73269349c0d7c40f6
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-11-01 15:59:00 +0900

    feat: selectbox icon추가

--- src/components/common/inputbox/select/Select.tsx
+++ src/components/common/inputbox/select/Select.tsx
@@ -18,15 +18,17 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
     ref
   ) => {
     return selectType === 'Single' ? (
-      <>
+      <div
+        className={`relative ${
+          fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
+        }`}
+      >
         <select
           ref={ref}
           name={name}
           id={''}
-          className={`${
-            fullWidth ? 'w-[324px] h-[52px]' : `w-[${width}px] h-[${height}px]`
-          } font-nsk body-18 px-[20px] border border-blue-500 rounded-[10px] outline-none bg-white-200
-      text-gray-600
+          className={`w-full h-full font-nsk body-18 px-[20px] border border-blue-500 rounded-[10px] outline-none bg-white-200
+      text-gray-600 appearance-none cursor-pointer
       `}
           onChange={onChange}
           {...props}
@@ -40,17 +42,17 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
             'pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'
           }
         >
-          <Icon icon={'ArrowDown'} />
+          <Icon icon={'ArrowDown'} classStyle={'text-gray-500'} />
         </div>
         <p className={'px-[10px] text-red-600'}>{errorMessage}</p>
-      </>
+      </div>
     ) : (
       <>
         <div
           className={`px-[23px] ${
             fullWidth ? 'w-[350px] h-[44px]' : `w-[${width}px] h-[${height}px]`
           } flex justify-between items-center border border-blue-500 rounded-[10px] font-nsk
-        bg-white-200
+        bg-white-200 appearance-none relative
         `}
         >
           <p className={'body-18 text-black-900'}>{'반복'}</p>
@@ -59,7 +61,7 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
             name={name}
             id={''}
             className={
-              'bg-white-200 body-14 text-gray-600 outline-none cursor-pointer relative'
+              'bg-white-200 body-14 text-gray-600 outline-none cursor-pointer relative appearance-none w-[50px]'
             }
             onChange={onChange}
             {...props}
@@ -68,6 +70,13 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
               <option value={optionValue}>{optionValue}</option>
             ))}
           </select>
+          <div
+            className={
+              'pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 right-[10px]'
+            }
+          >
+            <Icon icon={'ArrowDown'} classStyle={'text-gray-500'} />
+          </div>
         </div>
         <p className={'px-[10px] text-red-600'}>{errorMessage}</p>
       </>
