f3321014c152b11fe807f2c78921d82c8f6f96cf
kimheeseok <kim1140129@gmail.com>
2023-11-02 14:33:10 +0900

    feat: API 공통 파일 구현

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,30 @@
 import axios from 'axios'
 
-export const api = axios.create({
-  baseURL: 'baseUrl',
-  timeout: 10_000
+const request = axios.create({
+  baseURL: import.meta.env.VITE_API_ENDPOINT,
+  headers: {
+    Accept: '*/*'
+  }
 })
+
+request.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error) => {
+    console.error(error)
+    return Promise.reject(error)
+  }
+)
+
+request.interceptors.response.use(
+  (res) => {
+    return res
+  },
+  (error) => {
+    console.error(error)
+    return Promise.reject(error)
+  }
+)
+
+export default request
