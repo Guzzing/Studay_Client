e1755d98bd4f0c4c2988b8c4fd64ded74f9f40da
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-11-05 13:55:26 +0900

    feat: 로그인 메서드 정리

---
+++ src/api/autorization/LoginApi.ts
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import request from '..'
+
+const { VITE_CLIENT_ID, VITE_CLIENT_SECRET } = import.meta.env
+// 카카오 코드
+export const getCode = () => {
+  const currentURL = window.location.href
+  const params = new URLSearchParams(currentURL.split('?')[1])
+  const code = params.get('code')
+  return code as string
+}
+
+export const pushData = () => {
+  const data = new URLSearchParams()
+  data.append('grant_type', 'authorization_code')
+  data.append('client_id', VITE_CLIENT_ID)
+  data.append('redirect_uri', 'http://localhost:5173/login')
+  data.append('code', getCode())
+  data.append('client_secret', VITE_CLIENT_SECRET)
+
+  return data
+}
+export const getKaKaoAccessToken = async (data: URLSearchParams) => {
+  try {
+    const res = await request.post(
+      'https://kauth.kakao.com/oauth/token',
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+      }
+    )
+    return res.data.access_token
+  } catch {
+    throw new Error('cannot get kakao access token!')
+  }
+}
+
+export const getAccessToken = async (kakaoAccessToken: string) => {
+  try {
+    const res = await axios.get('http://3.114.43.57:8080/auth/kakao', {
+      headers: {
+        Authorization: `Bearer ${kakaoAccessToken}`
+      }
+    })
+    return res.data.appToken
+  } catch {
+    throw new Error('cannt get access token')
+  }
+}
