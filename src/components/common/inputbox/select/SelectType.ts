cb12e7113a1f6831aa7782031e3db60a6be1486d
Eugene Kim <eugene02876@gmail.com>
2023-11-06 14:48:16 +0900

    fix: selectBox 내용 수정

--- src/components/common/inputbox/select/SelectType.ts
+++ src/components/common/inputbox/select/SelectType.ts
@@ -3,13 +3,11 @@ import { ComponentProps } from 'react'
 type SelectType = 'Single' | 'Box'
 export interface SelectProps extends ComponentProps<'select'> {
   selectType: SelectType
-  optionData: string[]
   value: string
   options: string[]
   fullWidth?: boolean
   width?: number
   height?: number
   errorMessage?: string
-  options: string[]
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
