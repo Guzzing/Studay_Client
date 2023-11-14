8db9d270767d100902601097294a6fb678120842
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-11-14 17:55:27 +0900

    feat: 마이페이지 요청 시 userId제거

--- src/libs/api/mypage/myPageApi.ts
+++ src/libs/api/mypage/myPageApi.ts
@@ -1,14 +1,9 @@
-import type { MyPageResponse } from './myPageType'
+import type { GetMyPageResponse } from './myPageType'
 import request from '@/libs/api'
 
-export const myPageApi = async (): Promise<MyPageResponse> => {
+export const myPageApi = async (): Promise<GetMyPageResponse> => {
   try {
-    const userId = localStorage.getItem('userId')
-    const req = await request.get('http://3.114.43.57:8080/members/mypage', {
-      params: {
-        userId: Number(userId)
-      }
-    })
+    const req = await request.get('http://3.114.43.57:8080/members')
     console.log(req)
     return req.data
   } catch {
