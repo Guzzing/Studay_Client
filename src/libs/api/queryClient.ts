a853bdaf43904efa9accad4a1bf420d989e9781d
Eugene Kim <eugene02876@gmail.com>
2023-11-17 01:09:14 +0900

    chore: 쓸데없는 주석 제거

--- src/libs/api/queryClient.ts
+++ src/libs/api/queryClient.ts
@@ -9,9 +9,9 @@ export const queryClient = new QueryClient({
     }
   },
   queryCache: new QueryCache({
-    // onError: () => {
-    //   alert('에러 발생! 올바른 경로로 서비스를 이용해주세요.')
-    //   window.location.href = '/'
-    // }
+    onError: () => {
+      alert('에러 발생! 올바른 경로로 서비스를 이용해주세요.')
+      window.location.href = '/'
+    }
   })
 })
