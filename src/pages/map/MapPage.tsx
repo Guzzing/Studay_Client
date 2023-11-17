321a60e0af7a69a1ec1ea21edfe5048504ab3d0d
kimheeseok <kim1140129@gmail.com>
2023-11-17 18:34:45 +0900

    feat: 필터 페이지 개발

--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { useAtom } from 'jotai/index'
 import { SetLocationProps } from '../../types/mapPage.ts'
@@ -6,11 +6,16 @@ import Icon from '@/components/common/icon/Icon.tsx'
 import Input from '@/components/common/inputbox/input/Input.tsx'
 import Spacing from '@/components/common/spacing/Spacing.tsx'
 import NaverMap from '@/components/map/NaverMap.tsx'
+import { getAcademyFilter } from '@/libs/api/filter/filterApi.ts'
 import { getAcademyList } from '@/libs/api/mapapi/mapApi.ts'
 import { mapInfoAtom } from '@/libs/store/mapInfoAtom.ts'
 
 const MapPage = () => {
   const navigate = useNavigate()
+  const location = useLocation()
+  const queryString = location.search
+  console.log(queryString.split('&'))
+
   const [mapInfo, setMapInfo] = useAtom(mapInfoAtom)
   const { data: academyList } = useQuery({
     queryKey: ['academyList', mapInfo.latitude, mapInfo.longitude],
@@ -18,8 +23,16 @@ const MapPage = () => {
       getAcademyList({
         latitude: mapInfo.latitude,
         longitude: mapInfo.longitude
-      })
+      }),
+    enabled: !queryString
   })
+  const { data: academyFilterList } = useQuery({
+    queryKey: ['academyFilterList', queryString],
+    queryFn: () => getAcademyFilter({ queryString: queryString }),
+    enabled: queryString.length > 0
+  })
+
+  console.log(academyFilterList)
 
   const moveFilter = () => {
     navigate('/map/filter')
@@ -54,7 +67,11 @@ const MapPage = () => {
       <NaverMap
         latitude={mapInfo.latitude}
         longitude={mapInfo.longitude}
-        academyList={academyList?.academiesByLocationResponse || []}
+        academyList={
+          academyList?.academiesByLocationResponse ||
+          academyFilterList?.academyFilterResponses ||
+          []
+        }
         setLocation={setLocation}
       />
     </div>
