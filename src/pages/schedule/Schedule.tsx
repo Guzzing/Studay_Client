9d7ce5573ea5959df4c96fb1386104f932c84796
kimheeseok <kim1140129@gmail.com>
2023-11-21 01:38:13 +0900

    feat: 아이 스케줄 스타일 구현

--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { CalenderType } from '../../types/date.ts'
 import Calender from '@/components/common/calender/Calender.tsx'
+import ScheduleBox from '@/components/common/scheduleBox/ScheduleBox.tsx'
 import Spacing from '@/components/common/spacing/Spacing.tsx'
 import { defaultDate } from '@/libs/utils/date.ts'
 
@@ -32,6 +33,28 @@ const Schedule = () => {
           }
         ]}
       />
+      <div className={'flex flex-col scroll-auto'}>
+        <div className={'flex flex-row justify-center items-center mb-[28px]'}>
+          <span className={'w-[70px] body-14'}>{'오후 2시'}</span>
+          <div
+            className={'w-full h-[1px] border border-dashed border-t-black-800'}
+          />
+        </div>
+        <div className={'mb-[16px]'}>
+          <ScheduleBox
+            scheduleType={'profile'}
+            mainTitle={'닥스 어학원 - 청포도두개'}
+            subElement={'오후 4시에 종료'}
+          />
+        </div>
+        <div className={'mb-[16px]'}>
+          <ScheduleBox
+            scheduleType={'profile'}
+            mainTitle={'닥스 어학원 - 청포도두개'}
+            subElement={'오후 4시에 종료'}
+          />
+        </div>
+      </div>
     </div>
   )
 }
