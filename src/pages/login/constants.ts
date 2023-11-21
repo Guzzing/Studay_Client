53cdbf5ae0c4862f9246c6c702a8fd3128e9c373
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-11-21 18:42:56 +0900

    fix: 상수명 대문자로 변경

--- src/pages/login/constants.ts
+++ src/pages/login/constants.ts
@@ -4,12 +4,12 @@ import {
   VITE_GOOGLE_CLIENT_ID
 } from '../../constants/env'
 export const BUTTON_IMG_LINK = {
-  kakao: '/img/kakao.png',
-  google: '/img/google.png'
+  KAKAO: '/img/kakao.png',
+  GOOGLE: '/img/google.png'
 }
 
 // 아직 로그인 링크 못 구함
 export const LOGIN_LINK = {
-  kakao: `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_KAKAO_CLIENT_ID}&redirect_uri=${VITE_REDIRECT_URL}&response_type=code`,
-  google: `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&scope=email%20profile&client_id=${VITE_GOOGLE_CLIENT_ID}&redirect_uri=${VITE_REDIRECT_URL}`
+  KAKAO: `https://kauth.kakao.com/oauth/authorize?client_id=${VITE_KAKAO_CLIENT_ID}&redirect_uri=${VITE_REDIRECT_URL}&response_type=code`,
+  GOOGLE: `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&scope=email%20profile&client_id=${VITE_GOOGLE_CLIENT_ID}&redirect_uri=${VITE_REDIRECT_URL}`
 }
