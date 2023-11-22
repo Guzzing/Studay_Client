2ad6a7339cbff1228f5cae204117ef9d6e39f5b2
kimheeseok <kim1140129@gmail.com>
2023-11-22 15:40:31 +0900

    feat: Icon 클릭 이벤트 추가 및 CSS 정렬 수정

--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { CalenderType } from '../../types/date.ts'
 import Calender from '@/components/common/calender/Calender.tsx'
 import Icon from '@/components/common/icon/Icon.tsx'
@@ -7,6 +8,7 @@ import Spacing from '@/components/common/spacing/Spacing.tsx'
 import { defaultDate } from '@/libs/utils/date.ts'
 
 const Schedule = () => {
+  const navigate = useNavigate()
   const [defaultYear, defaultMonth, defaultDays, days] = defaultDate()
   const [calenderState, setCalenderState] = useState<CalenderType>({
     nowYear: defaultYear,
@@ -43,14 +45,14 @@ const Schedule = () => {
             className={'w-full h-[1px] border border-dashed border-t-black-800'}
           />
         </div>
-        <div className={'mb-[16px]'}>
+        <div className={'flex mb-[16px] justify-center items-center'}>
           <ScheduleBox
             scheduleType={'profile'}
             mainTitle={'닥스 어학원 - 청포도두개'}
             subElement={'오후 4시에 종료'}
           />
         </div>
-        <div className={'mb-[16px]'}>
+        <div className={'flex mb-[16px] justify-center items-center'}>
           <ScheduleBox
             scheduleType={'profile'}
             mainTitle={'닥스 어학원 - 청포도두개'}
@@ -61,8 +63,9 @@ const Schedule = () => {
       <Icon
         icon={'Add'}
         classStyle={
-          'cursor-pointer absolute left-[80%] top-1/3 w-[60px] h-[60px]'
+          'cursor-pointer absolute left-[85%] top-[80%] w-[60px] h-[60px]'
         }
+        onClick={() => navigate('/schedule/new')}
       />
     </div>
   )
