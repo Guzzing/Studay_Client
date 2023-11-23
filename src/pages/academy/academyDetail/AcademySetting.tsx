c3419d89756ba8cfcc801ad304f78b542eb7a496
Eugene Kim <eugene02876@gmail.com>
2023-11-23 18:23:24 +0900

    feat: 학원 그만두기, 등록하기 기능 구현

--- src/pages/academy/academyDetail/AcademySetting.tsx
+++ src/pages/academy/academyDetail/AcademySetting.tsx
@@ -1,6 +1,20 @@
+import { useNavigate } from 'react-router-dom'
+import BottomSheet from '@/components/common/bottomsheet/BottomSheet'
 import Button from '@/components/common/button/Button'
+import { deleteDashboard } from '@/libs/api/dashboard/DashBoardApi'
+import { patchToggleDashboardState } from '@/libs/api/dashboard/DashBoardApi'
 import { GetAllDashBoardResponse } from '@/libs/api/dashboard/DashBoardType'
+import { queryClient } from '@/libs/api/queryClient'
 const AcademySetting = ({ data }: { data: GetAllDashBoardResponse }) => {
+  const navigate = useNavigate()
+  // eslint-disable-next-line unicorn/consistent-function-scoping
+  const deleteDashboardInfo = async (dashboardId: number) => {
+    const data = await deleteDashboard(dashboardId)
+    return data
+  }
+  const fetchToggleDashboard = async (dashboardId: number) => {
+    const res = await patchToggleDashboardState(dashboardId)
+  }
   return (
     <div className={'w-full mt-[37px] px-[20px]'}>
       {data.isActive ? (
@@ -14,6 +28,13 @@ const AcademySetting = ({ data }: { data: GetAllDashBoardResponse }) => {
             fullWidth={true}
             buttonType={'Plain-red'}
             label={'학원을 그만뒀어요'}
+            onClick={() => {
+              fetchToggleDashboard(data.dashboardId)
+              queryClient.invalidateQueries({
+                queryKey: ['dashboard', data.dashboardId]
+              })
+              alert('성공적으로 처리했음.')
+            }}
           />
         </div>
       ) : (
@@ -22,11 +43,27 @@ const AcademySetting = ({ data }: { data: GetAllDashBoardResponse }) => {
             buttonType={'Plain-blue'}
             label={'학원을 등록했어요'}
             fullWidth={true}
+            onClick={() => {
+              fetchToggleDashboard(data.dashboardId)
+              queryClient.invalidateQueries({
+                queryKey: ['dashboard', data.dashboardId]
+              })
+              alert('성공적으로 처리했음.')
+            }}
           />
           <Button
             buttonType={'Plain-red'}
             label={'삭제하기'}
             fullWidth={true}
+            onClick={() => {
+              if (data.isActive) {
+                alert('다니고 있는 학원은 삭제가 불가능합니다!')
+              } else {
+                deleteDashboardInfo(data.dashboardId)
+                alert('삭제 완료!')
+                navigate('/academies')
+              }
+            }}
           />
         </div>
       )}
