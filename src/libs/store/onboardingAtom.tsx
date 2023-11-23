7c7e16539d12df4573d64f1a5ff6aba3254ea230
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-11-23 22:24:30 +0900

    feat: onboarding atom

--- src/libs/store/onboardingAtom.tsx
+++ src/libs/store/onboardingAtom.tsx
@@ -14,7 +14,11 @@ const initialPageValuesAtom = atom<PostOnboardingRequest>({
 
 const initialCurPage = atom(0)
 const isSubmitToggle = atom(false)
+const toggleDone = atom(false)
+const storage = atom<string[]>([])
 
-export const onboardingPageData = atom(initialPageValuesAtom).init
-export const initialCurPageNumber = atom(initialCurPage).init
-export const isSubmit = atom(isSubmitToggle).init
+export const onboardingPageDataAtom = atom(initialPageValuesAtom).init
+export const initialCurPageNumberAtom = atom(initialCurPage).init
+export const isSubmitAtom = atom(isSubmitToggle).init
+export const isDoneAtom = atom(toggleDone).init
+export const storeStorageAtom = atom(storage).init
