618b250e5563c9639b3015efde5a2cba49b5648e
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-11-23 22:24:48 +0900

    feat: 글자 라인 맞추기위해 배열로 설정

--- src/pages/onboarding/constants.ts
+++ src/pages/onboarding/constants.ts
@@ -1,24 +1,24 @@
 export const PAGE_CONTENT = [
   {
     type: 'nickname',
-    mainTitle: '안녕하세요 :) 스터데이에요',
-    subTitle: '스터데이의 커뮤니티 문화를 위해 매너있는 이름을 써주세요',
+    mainTitle: ['안녕하세요 :)', '스터데이에요'],
+    subTitle: ['스터데이의 커뮤니티 문화를 위해', '매너있는 이름을 써주세요'],
     step: [1],
     inputTitle: ['사용할 닉네임을 입력해주세요'],
     buttonType: ['입력완료', '']
   },
   {
     type: 'email',
-    mainTitle: '스터데이의 최근 소식을 알려드리고 싶어요.',
-    subTitle: '작성해주신 이메일로 최신 정보를 보내드려요',
+    mainTitle: ['스터데이의 최근 소식을', '알려드리고 싶어요.'],
+    subTitle: ['작성해주신 이메일로 최신 정보를 보내드려요'],
     step: [2],
     inputTitle: ['이메일을 입력해주세요'],
     buttonType: ['입력완료', '']
   },
   {
     type: 'first-child',
-    mainTitle: '자녀에 대해서 알려주세요',
-    subTitle: '자녀별로 시간표를 관리하고 맞춤 정보를 관리할 수 있어요',
+    mainTitle: ['자녀에 대해서', '알려주세요'],
+    subTitle: ['자녀별로 시간표를 관리하고', '맞춤 정보를 관리할 수 있어요'],
     step: [3, 4],
     inputTitle: [
       '아이의 이름(애칭)을 알려주세요',
@@ -31,8 +31,8 @@ export const PAGE_CONTENT = [
   },
   {
     type: 'first-child',
-    mainTitle: '둘째는 어떤 아이인가요?',
-    subTitle: '자녀별로 시간표를 관리하고 맞춤 정보를 관리할 수 있어요',
+    mainTitle: ['둘째는 어떤', '아이인가요?'],
+    subTitle: ['자녀별로 시간표를 관리하고', '맞춤 정보를 관리할 수 있어요'],
     step: [3, 4],
     inputTitle: [
       '아이의 이름(애칭)을 알려주세요',
@@ -45,8 +45,8 @@ export const PAGE_CONTENT = [
   },
   {
     type: 'first-child',
-    mainTitle: '귀염둥이 셋째! 정보를 입력해주세요',
-    subTitle: '자녀별로 시간표를 관리하고 맞춤 정보를 관리할 수 있어요',
+    mainTitle: ['귀염둥이 셋째!', '정보를 입력해주세요'],
+    subTitle: ['자녀별로 시간표를 관리하고', '맞춤 정보를 관리할 수 있어요'],
     step: [3, 4],
     inputTitle: [
       '아이의 이름(애칭)을 알려주세요',
@@ -59,8 +59,8 @@ export const PAGE_CONTENT = [
   },
   {
     type: 'first-child',
-    mainTitle: '우와, 벌써 넷째에요',
-    subTitle: '자녀별로 시간표를 관리하고 맞춤 정보를 관리할 수 있어요',
+    mainTitle: ['우와,', '벌써 넷째에요'],
+    subTitle: ['자녀별로 시간표를 관리하고', '맞춤 정보를 관리할 수 있어요'],
     step: [3, 4],
     inputTitle: [
       '아이의 이름(애칭)을 알려주세요',
@@ -73,8 +73,8 @@ export const PAGE_CONTENT = [
   },
   {
     type: 'first-child',
-    mainTitle: '마지막이에요! 다섯째는요?',
-    subTitle: '자녀별로 시간표를 관리하고 맞춤 정보를 관리할 수 있어요',
+    mainTitle: ['마지막이에요!', '다섯째는요?'],
+    subTitle: ['자녀별로 시간표를 관리하고', '맞춤 정보를 관리할 수 있어요'],
     step: [3, 4],
     inputTitle: [
       '아이의 이름(애칭)을 알려주세요',
@@ -85,6 +85,7 @@ export const PAGE_CONTENT = [
 ]
 
 export const CHILD_GRADE = [
+  '',
   '초등학교 1학년',
   '초등학교 2학년',
   '초등학교 3학년',
