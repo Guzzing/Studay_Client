e4a2d3d448d182dd4681a40570d2a51ea9eb32f0
kimheeseok <kim1140129@gmail.com>
2023-11-26 23:48:59 +0900

    feat: onClick 이벤트 제거

--- src/components/common/calender/CalenderType.ts
+++ src/components/common/calender/CalenderType.ts
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CalenderType, DaysInfo } from '../../../types/date.ts'
+import { CalenderType } from '../../../types/date.ts'
 import { Holidays } from '@/libs/api/schedule/scheduleType.ts'
 
 interface CalenderProps {
@@ -7,7 +7,6 @@ interface CalenderProps {
   setCalenderState: React.Dispatch<React.SetStateAction<CalenderType>>
   existenceDays: number[]
   holidays: Holidays[] | []
-  onClick: (daysInfo: DaysInfo) => void
 }
 
 export default CalenderProps
