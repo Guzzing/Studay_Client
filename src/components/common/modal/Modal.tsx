54f05251f1206da92fbc06c476d2cfe097506927
kimheeseok <kim1140129@gmail.com>
2023-11-26 23:51:48 +0900

    feat: Modal 이벤트 전파 방지코드 추가

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -10,11 +10,12 @@ const Modal = ({ children, onClose }: ModalProperties) => {
     <div
       id={'background'}
       className={
-        'fixed absolute left-0 top-0 w-full h-screen bg-[rgba(0,0,0,0.2)] z-50'
+        'fixed absolute left-0 top-0 w-full h-screen bg-[rgba(0,0,0,0.2)]'
       }
       onClick={onClose}>
       <div
-        className={`absolute left-1/2 top-1/2 text-center z-10 translate-y-[-50%] translate-x-[-50%]`}>
+        className={`absolute left-1/2 top-1/2 text-center z-50 translate-y-[-50%] translate-x-[-50%]`}
+        onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
