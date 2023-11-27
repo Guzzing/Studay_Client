b3fe19e314b549316b4b0bfbd6f56e2359c88a5c
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-11-27 15:11:03 +0900

    햄버거 로고 클릭 시 settingPage 토클링 (#184)
    * feat: useSidebar기능 생성
    
    * feat: header sidebar onclick 활성화
    
    * feat: toggle 전역 state로 관리
    
    * feat: calendar페이지 생성
    
    * feat: 더미데이터 수정
    
    * feat: 세팅페이지 토글 누르면 나오게 생성
    
    * feat: 세팅페이지 라우팅에서 제거
    
    * chore: setting page슬라이드를 위한 react-transition-group설치
    
    * useSidebar hook생성
    
    * feat: myPage setting페이지 슬라이드 적용
    
    * feat: setting slide css, CSSTransition적용
    
    * feat: 헤더 Logo type에 useSidebar적용
    
    * feat: 페이지 이동시 다시 sidebar가 들어가게 설정
    
    * feat: sidebar모든 페이지에 적용
    
    * fix: settingPage url에서 제거
    
    * feat: npm i 진행

---
+++ src/pages/calendar/CalendarPage.tsx
@@ -0,0 +1,176 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Accordion } from '@/components/common/accordion/Accordion'
+import Button from '@/components/common/button/Button'
+import Icon from '@/components/common/icon/Icon'
+import Label from '@/components/common/label/Label'
+import ListRow from '@/components/common/listRow/ListRow'
+import Modal from '@/components/common/modal/Modal'
+import Profile from '@/components/common/profile/Profile'
+import Spacing from '@/components/common/spacing/Spacing'
+import { DATA } from '@/pages/calendar/data'
+
+const CalendarPage = () => {
+  // const { calendarId } = useParams()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isDeleteModalOpen, setisDeleteModalOpen] = useState(false)
+
+  return (
+    <div className={'w-full h-full border px-[30px]'}>
+      <Spacing size={110} />
+      <div className={'relative h-[194px] w-full'}>
+        <h2 className={'headline-25 py-[5px]'}>
+          {DATA.academyInfo.academyName}
+        </h2>
+        <Icon
+          icon={'Edit'}
+          classStyle={'absolute top-0 right-0 cursor-pointer'}
+        />
+        <p className={'body-14 py-[5px]'}>{DATA.date}</p>
+        <div className={'flex items-center caption-13 py-[5px]'}>
+          <Icon icon={'Time'} />
+          <span
+            className={
+              'pl-[5px]'
+            }>{`${DATA.lessonInfo.lessonTimes[0].startTime} - ${DATA.lessonInfo.lessonTimes[0].endTime}`}</span>
+        </div>
+        <p className={'caption-13'}>{'반복: 매주 목요일, 토요일'}</p>
+        <div className={'flex items-center body-14 pt-[15px]'}>
+          <Icon icon={'MapPin'} />
+          <p>{DATA.academyInfo.address}</p>
+        </div>
+        <div className={'absolute right-0 bottom-[45px]'}>
+          <Label variant={'medium'} label={'국어'} icon={'Korean'} />
+        </div>
+      </div>
+      <Accordion
+        title={DATA.lessonInfo.lessonName}
+        content={
+          <div>
+            <ListRow
+              leftElement={
+                <div className={'flex'}>
+                  <span className={'px-[5px]'}>{'금액'}</span>
+                </div>
+              }
+              rightElement={
+                <span>
+                  {DATA.lessonInfo.total_fee}
+                  {'원'}
+                </span>
+              }
+              className={'cursor-pointer'}
+            />
+            <ListRow
+              leftElement={
+                <div className={'flex'}>
+                  <span className={'px-[5px]'}>{'정원'}</span>
+                </div>
+              }
+              rightElement={<span>{DATA.lessonInfo.capacity}</span>}
+              className={'cursor-pointer'}
+            />
+          </div>
+        }
+        rightElement={<Icon icon={'ArrowDown'} />}
+        initialState={true}
+        contentHeight={100}
+      />
+      <div className={'py-[12px]'}>
+        <h2 className={'subhead-18'}>{'일정 수행중인 아이'}</h2>
+        <div className={'flex items-center justify-start pt-[10px]'}>
+          {DATA.childrenInfos.map(({ childId, childName, memo }) => (
+            <li key={childId} className={'list-none'}>
+              <Profile imageSize={'M'} imageLabel={childName} />
+            </li>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2 className={'subhead-18'}>{'메모'}</h2>
+        <div className={'h-[70px] overflow-auto'}>
+          {DATA.childrenInfos.map(({ childId, memo }) => (
+            <li
+              key={childId}
+              className={
+                'list-none flex items-center justify-start overflow-scroll'
+              }>
+              <Icon icon={'Edit'} />
+              {memo}
+            </li>
+          ))}
+        </div>
+      </div>
+      <div className={'absolute left-[30px] bottom-[28px]'}>
+        <Button
+          buttonType={'Plain-blue'}
+          label={'학원정보 보러가기'}
+          onClick={() => setIsModalOpen(true)}
+        />
+        <Button
+          buttonType={'Plain-red'}
+          label={'일정 삭제하기'}
+          className={'mt-[10px]'}
+          onClick={() => setisDeleteModalOpen(true)}
+        />
+      </div>
+      {isModalOpen && (
+        <Modal>
+          <div
+            className={
+              'w-[340px] h-[240px] px-[34px] py-[30px] flex flex-col bg-white-0 justify-around items-center rounded-[10px]'
+            }>
+            <p className={'subHead-18'}>{'어떤 아이의 정보를 확인할까요?'}</p>
+            <div className={'flex items-center justify-start pt-[10px]'}>
+              {DATA.childrenInfos.map(({ childId, childName, memo }) => (
+                <li key={childId} className={'list-none'}>
+                  <Profile imageSize={'M'} />
+                </li>
+              ))}
+            </div>
+            <Button
+              buttonType={'Plain-blue'}
+              label={'닫기'}
+              fullWidth={false}
+              width={'MW'}
+              className={'w-[220px]'}
+              onClick={() => setIsModalOpen(false)}
+            />
+          </div>
+        </Modal>
+      )}
+      {isDeleteModalOpen && (
+        <Modal>
+          <div
+            className={
+              'w-[340px] h-[240px] px-[30px] py-[30px] flex flex-col bg-white-0 justify-around items-center rounded-[10px]'
+            }>
+            <p className={'subHead-18'}>{'일정을 삭제할까요?'}</p>
+            <div
+              className={'flex flex-col items-center justify-around pt-[10px]'}>
+              <Button
+                buttonType={'Plain-red'}
+                label={'오늘 일정만 삭제'}
+                className={'mb-[10px]'}
+                fullWidth={false}
+                width={'SW'}
+              />
+              <Button
+                buttonType={'Plain-red'}
+                label={'이후 모든일정 삭제'}
+                className={'mb-[10px]'}
+              />
+              <Button
+                buttonType={'Plain-blue'}
+                label={'취소'}
+                onClick={() => setisDeleteModalOpen(false)}
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </div>
+  )
+}
+
+export default CalendarPage
--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,19 +1,21 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAtom } from 'jotai'
+import SettingPage from '../setting/SettingPage'
 import Button from '@/components/common/button/Button'
-import Header from '@/components/common/header/Header'
 import Icon from '@/components/common/icon/Icon'
 import ListRow from '@/components/common/listRow/ListRow'
 import Profile from '@/components/common/profile/Profile'
 import Spacing from '@/components/common/spacing/Spacing'
 import { logoutApi } from '@/libs/api/autorization/logout/LogoutApi'
 import { myPageApi } from '@/libs/api/mypage/myPageApi'
+import useSidebar from '@/libs/hooks/useSidebar'
 import { myPageAtom } from '@/libs/store/myPageAtom'
 
 const MyPage = () => {
   const navigate = useNavigate()
   const [myPageData, setMyPageData] = useAtom(myPageAtom)
+  const { toggleOpen } = useSidebar()
   useEffect(() => {
     if (localStorage.getItem('token') === null) {
       alert('로그인 페이지로 이동합니다!')
@@ -26,65 +28,69 @@ const MyPage = () => {
     }
     response()
   }, [])
+
   return (
-    <div className={'relative h-full'}>
-      <Header headerType={'Logo'} pageTitle={'마이페이지'} />
-      <Spacing size={80} />
-      <div className={'h-[110px] pl-[25px] py-[30px] headline-20'}>
-        <h2>{`${myPageData?.nickname}님 안녕하세요!`}</h2>
-        <p className={'body-15-gray py-[5px]'}>{myPageData?.email}</p>
-      </div>
-      <div className={'h-[175px] p-[20px]'}>
-        <div className={'flex items-center mb-[5px]'}>
-          <span>{'내 아이 관리하기'}</span>
-          <Icon
-            icon={'Add'}
-            classStyle={'w-[30px] h-[30px] cursor-pointer'}
-            onClick={() =>
-              myPageData.childInformationResponses.length === 5
-                ? alert('아이가 꽉 찼습니다!')
-                : navigate('/onboarding')
-            }
+    <div className={'relative h-full overflow-hidden'}>
+      <SettingPage isOpen={toggleOpen} />
+      <div>
+        <Spacing size={80} />
+        <div className={'h-[110px] pl-[25px] py-[30px] headline-20'}>
+          <h2>{`${myPageData?.nickname}님 안녕하세요!`}</h2>
+          <p className={'body-15-gray py-[5px]'}>{myPageData?.email}</p>
+        </div>
+        <div className={'h-[175px] p-[20px]'}>
+          <div className={'flex items-center mb-[5px]'}>
+            <span>{'내 아이 관리하기'}</span>
+            <Icon
+              icon={'Add'}
+              classStyle={'w-[30px] h-[30px] cursor-pointer'}
+              onClick={() =>
+                myPageData.childInformationResponses.length === 5
+                  ? alert('아이가 꽉 찼습니다!')
+                  : navigate('/onboarding')
+              }
+            />
+          </div>
+          <div className={'flex overflow-x-scroll'}>
+            {myPageData.childInformationResponses.map(
+              ({ childId, childName }) => (
+                <li
+                  key={childId}
+                  className={`list-none px-[10px] flex-shrink-0`}>
+                  <Profile
+                    imageSize={'M'}
+                    imageLabel={childName}
+                    canEdit={true}
+                    onClick={() =>
+                      navigate(`/edit/${childId}`, { state: childId })
+                    }
+                  />
+                </li>
+              )
+            )}
+          </div>
+        </div>
+        <ListRow
+          leftElement={<p>{'찜한 학원 보러가기'}</p>}
+          rightElement={
+            <Icon icon={'ArrowDown'} classStyle={'rotate-[-90deg]'} />
+          }
+          onClick={() => navigate('/likeacademy')}
+          className={'py-[20px] cursor-pointer'}
+        />
+        <div className={'absolute bottom-[38px] flex flex-col mx-8'}>
+          <Button
+            buttonType={'Plain-blue'}
+            label={'로그아웃 하기'}
+            className={'mb-[20px]'}
+            onClick={() => logoutApi()}
+          />
+          <Button
+            buttonType={'Plain-red'}
+            label={'회원탈퇴 하기'}
+            onClick={() => alert('회원 탈퇴하기!')}
           />
         </div>
-        <div className={'flex overflow-x-scroll'}>
-          {myPageData.childInformationResponses.map(
-            ({ childId, childName, childProfileImageUrl }) => (
-              <li key={childId} className={`list-none px-[10px] flex-shrink-0`}>
-                <Profile
-                  imageUrl={childProfileImageUrl}
-                  imageSize={'M'}
-                  imageLabel={childName}
-                  canEdit={true}
-                  onClick={() =>
-                    navigate(`/edit/${childId}`, { state: childId })
-                  }
-                />
-              </li>
-            )
-          )}
-        </div>
-      </div>
-      <ListRow
-        leftElement={<p>{'찜한 학원 보러가기'}</p>}
-        rightElement={
-          <Icon icon={'ArrowDown'} classStyle={'rotate-[-90deg]'} />
-        }
-        onClick={() => navigate('/likeacademy')}
-        className={'py-[20px] cursor-pointer'}
-      />
-      <div className={'absolute bottom-[38px] flex flex-col mx-8'}>
-        <Button
-          buttonType={'Plain-blue'}
-          label={'로그아웃 하기'}
-          className={'mb-[20px]'}
-          onClick={() => logoutApi()}
-        />
-        <Button
-          buttonType={'Plain-red'}
-          label={'회원탈퇴 하기'}
-          onClick={() => alert('회원 탈퇴하기!')}
-        />
       </div>
     </div>
   )
