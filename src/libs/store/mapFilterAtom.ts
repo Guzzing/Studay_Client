612877a08e05b94edfbf7927d6c59f1c88b3b151
kimheeseok <kim1140129@gmail.com>
2023-11-29 14:42:01 +0900

    hotfix: 필터 페이지 수정

--- src/libs/store/mapFilterAtom.ts
+++ src/libs/store/mapFilterAtom.ts
@@ -2,16 +2,15 @@ import { atom } from 'jotai'
 
 const initMapFilter: SubjectList = {
   subjectList: [
-    { title: '예능', filter: '예능(대)', color: 'default' },
-    { title: '국제화', filter: '국제화', color: 'default' },
-    { title: '입시', filter: '입시, 검정 및 보습', color: 'default' },
-    { title: '직업기술', filter: '직업기술', color: 'default' },
-    { title: '종합', filter: '종합(대)', color: 'default' },
-    { title: '독서실', filter: '독서실', color: 'default' },
-    { title: '기예', filter: '기예(대)', color: 'default' },
-    { title: '기타', filter: '기타(대)', color: 'default' },
-    { title: '인문사회', filter: '인문사회(대)', color: 'default' },
-    { title: '정보', filter: '정보', color: 'default' }
+    { title: '수학', filter: '수학', color: 'default' },
+    { title: '과학', filter: '과학', color: 'default' },
+    { title: '국어', filter: '국어', color: 'default' },
+    { title: '영어', filter: '영어', color: 'default' },
+    { title: '컴퓨터', filter: '컴퓨터', color: 'default' },
+    { title: '예체능', filter: '예체능', color: 'default' },
+    { title: '외국어', filter: '외국어', color: 'default' },
+    { title: '보습', filter: '보습', color: 'default' },
+    { title: '기타', filter: '기타', color: 'default' }
   ],
   maxMoney: 500_000,
   minMoney: 1
