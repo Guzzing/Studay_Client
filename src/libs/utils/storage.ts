204a075e7878663a42cf135f05b51d3a160613f7
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-12-01 00:25:31 +0900

    refactor: 스토리지 getItem 문자열 예외처리

--- src/libs/utils/storage.ts
+++ src/libs/utils/storage.ts
@@ -3,7 +3,11 @@ export const setItem = (key: string, value: string) => {
 }
 
 export const getItem = (key: string) => {
-  return JSON.parse(localStorage.getItem(key) as string) || []
+  const storedItem = localStorage.getItem(key)
+  console.log(storedItem, typeof storedItem)
+  if (storedItem === undefined || storedItem === null) return []
+  else if (typeof storedItem === 'string') return storedItem
+  return JSON.parse(localStorage.getItem(key) as string)
 }
 
 export const getStorageLength = (key: string) => {
