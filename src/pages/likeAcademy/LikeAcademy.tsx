fd47514232c26bdc237e60184eb19eb64a60fd92
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-12-01 17:40:00 +0900

    fix: 찜한 학원이 없을 때 없다는 문장 추가

--- src/pages/likeAcademy/LikeAcademy.tsx
+++ src/pages/likeAcademy/LikeAcademy.tsx
@@ -53,7 +53,7 @@ const LikeAcademy = () => {
       <Spacing size={90} />
       <SettingPage isOpen={toggleOpen} />
       <div className={'overflow-auto h-[600px]'}>
-        {likeAcademies?.likeAcademyInfos &&
+        {likeAcademies?.likeAcademyInfos.length ? (
           likeAcademies.likeAcademyInfos?.map(
             ({ likeId, academyId, academyName, expectedFee }, index) => (
               <li
@@ -107,7 +107,13 @@ const LikeAcademy = () => {
                 />
               </li>
             )
-          )}
+          )
+        ) : (
+          <div className={'flex justify-center items-center'}>
+            <Spacing size={600} />
+            <p className={'body-18-gray'}>{'찜한 학원이 아직 없습니다'}</p>
+          </div>
+        )}
 
         <div
           className={
