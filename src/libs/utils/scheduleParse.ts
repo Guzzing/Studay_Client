af7a6681276af7126fe1ba2d017fb46927e50a86
Eugene Kim <eugene02876@gmail.com>
2023-12-04 01:39:52 +0900

    feat: 메인페이지에 자식 상태 추가

---
+++ src/libs/utils/scheduleParse.ts
@@ -0,0 +1,10 @@
+import { ChildrenScheduleInfo } from '@/libs/api/children/ChildrenType'
+export const scheduleParse = (
+  schedule: ChildrenScheduleInfo,
+  childName: string
+) => {
+  if (schedule.academyName === '수행 중인 학원이 없습니다.') {
+    return `${childName}는 현재 수행 중인 일정이 없어요.`
+  }
+  return schedule.academyName
+}
