eb749ede1596f307c8b14fb3ccd584b447108fdc
HeeSeok-kim <106604926+HeeSeok-kim@users.noreply.github.com>
2023-12-05 00:45:33 +0900

    법정시간에 따른 아이 학원 제한시간 추가 및 스케줄 페이지 버그 수정 (#333)
    * fix: 가격format 학원 list에도 적용
    
    * fix: schedule 스크롤
    
    * feat: 프로그래시브 웹 앱 개선
    
    * fix: label까지 header안에 넣음 => 겹침 방지
    
    * fix: x축 scroll현상 막음
    
    * feat: 메타 아이콘 추가
    
    * fix: 온보딩 버튼 문제 해결
    
    * fix: 항상 첫번째 아이로 고정되는 현상 해결
    
    * fix: 맵 페이지 바텀시트 버그 수정 (#330)
    
    * fix: 제한시간 추가
    
    ---------
    
    Co-authored-by: 신서리티코더 <85999976+jisung24@users.noreply.github.com>
    Co-authored-by: Eugene Kim <eugene02876@gmail.com>

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -45,6 +45,12 @@ const Button = ({
         } ${height ? 'h-[43px]' : BUTTON_HEIGHT[height]} ${
           BUTTON_BG_COLOR['white0']
         } ${BUTTON_TEXT_COLOR['gray500']} ${BUTTON_BORDER_COLOR['gray500']}`
+      : buttonType === 'Plain-Onboarding-disabled'
+      ? `${BUTTON_RADIUS['middle']} ${
+          fullWidth ? 'w-full' : width ? 'w-[343px]' : BUTTON_WIDTH[width]
+        } ${height ? 'h-[56px]' : BUTTON_HEIGHT[height]} ${
+          BUTTON_BG_COLOR['white0']
+        } ${BUTTON_TEXT_COLOR['gray500']} ${BUTTON_BORDER_COLOR['gray500']}`
       : buttonType === 'Plain-red'
       ? `${BUTTON_RADIUS['middle']} ${
           fullWidth ? 'w-full' : width ? 'w-[317px]' : BUTTON_WIDTH[width]
